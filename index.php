d9ef22897eb5fde1f1c5868fa421fe5b11ca4d34
DamirPavlik <damir.pavlik@gmail.com>
2024-05-06 01:17:26 +0200

    feat: main page

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+
+use app\Models\Auction;
+
+require_once __DIR__ . "/vendor/autoload.php";
+require_once "app/Models/User.php";
+
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+//$auctionModel = new Auction();
+//
+//for($i = 0; $i < 20; $i++) {
+//    $auctionModel->createAuction(productId: rand(1, 4), owner: rand(1,2));
+//}
