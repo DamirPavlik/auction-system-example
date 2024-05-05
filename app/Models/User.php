7953e2f2674aa9bfc5149516a8f9109e85e16383
DamirPavlik <damir.pavlik@gmail.com>
2024-05-06 00:08:21 +0200

    feat: products model

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,8 +5,23 @@ require_once "BaseModel.php";
 
 class User extends BaseModel
 {
-    public function test(): void
+    public function __construct()
     {
-        echo "hello world";
+        parent::__construct();
+    }
+
+    public function userExists(string $email): bool
+    {
+        $email = $this->sql->real_escape_string($email);
+        $response = $this->sql->query("SELECT id FROM users WHERE email='$email' LIMIT 1");
+        return $response->num_rows >= 1;
+    }
+
+    public function register(string $email, string $password): void
+    {
+        $password = password_hash(password: $password, algo: PASSWORD_DEFAULT);
+        $email = $this->sql->real_escape_string($email);
+        $password = $this->sql->real_escape_string($password);
+        $this->sql->query("INSERT INTO users (email, password) VALUES ('$email','$password')");
     }
 }
